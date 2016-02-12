40096a80df90eb9143f28c1a6f526b29adab12ef
Lucas Machado <lucas.dpmachado@gmail.com>
2016-02-12 08:57:34 -0800

    Final Fixes to the branch

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
-  has_attached_file :upload, styles: { medium: "300x300>", thumb: "100x100>"  }, default_url: "/images/:style/missing.png"
+  has_attached_file :upload, styles: { medium: "300x300>", thumb: "100x100>"  }
   validates_attachment_content_type :upload, content_type: /\Aimage\/.*\Z/
 
   validates :recipe, :kitchen, :food, :kind, :ingredients, :steps, presence: true
