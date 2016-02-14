3a2dc326c1f119c23c839378993993f3cf2f63c6
Lucas Machado <lucas.dpmachado@gmail.com>
2016-02-14 11:35:33 -0800

    Fix Recent Recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,8 +2,8 @@ class Recipe < ActiveRecord::Base
   has_many :kitchens
   has_many :meals
   has_many :kinds
-  has_attached_file :upload, styles: { medium: '300x300>', thumb: '100x100>' }
-                    #:default_url => ":style/missing_avatar.png"
+  has_attached_file :upload, styles: { medium: '300x300>', thumb: '100x100>' },
+                    :default_url => ":style/missing_avatar.png"
   validates_attachment_content_type :upload, content_type: /\Aimage\/.*\Z/
 
   validates :recipe, :kitchen, :meal, :kind,
